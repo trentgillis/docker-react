3146c5627d3b5c3a0d49a99b278602ae26d11ffd
Trentin Gillis <trentin.gillis@gmail.com>
2020-08-24 13:06:48 -0600

    A change

--- src/App.js
+++ src/App.js
@@ -8,7 +8,7 @@ function App() {
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p>
-          I was changed!
+          Pushing for Travis :)
         </p>
         <a
           className="App-link"
